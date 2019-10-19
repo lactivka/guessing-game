c8afdb5c5d95a735e8eec887f637800dd85d40ff
Stepanenko Oksana <lactivo4ka@gmail.com>
2019-10-19 23:29:24 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+         
+    }
+    
+    setRange(min, max) { // constructor with properties min and max
+        this.min = min;
+        this.max = max;   
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.max + this.min)/2); // average value of min and max
+        return this.middle;
     }
 
     lower() {
-
+      this.setRange(this.min, this.middle); // if average value is greater than answer
     }
 
     greater() {
-
+       this.setRange(this.middle, this.max); // if average value is lower than answer
     }
 }
 
